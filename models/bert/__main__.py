4b51605c223e3784bec7f72583776b9969117fc2
Elisa Ferracane <elisa@ferracane.com>
2020-04-14 14:15:21 -0500

    fix weight decay setting

--- models/bert/__main__.py
+++ models/bert/__main__.py
@@ -109,7 +109,8 @@ if __name__ == '__main__':
     param_optimizer = list(model.named_parameters())
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay': args.weight_decay},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
 
     if args.fp16:
@@ -129,7 +130,7 @@ if __name__ == '__main__':
             optimizer = FP16_Optimizer(optimizer, static_loss_scale=args.loss_scale)
 
     else:
-        optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, weight_decay=args.weight_decay, correct_bias=False)
+        optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, correct_bias=False)
         scheduler = get_linear_schedule_with_warmup(optimizer, num_training_steps=num_train_optimization_steps,
                                                     num_warmup_steps=args.warmup_proportion * num_train_optimization_steps)
 
