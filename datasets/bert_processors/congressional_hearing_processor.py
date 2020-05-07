3d672704b8c53ad36a7c259e00489370ea3b7cf3
Elisa Ferracane <elisa@ferracane.com>
2020-05-07 15:58:00 -0500

    optionally use text_b

--- datasets/bert_processors/congressional_hearing_processor.py
+++ datasets/bert_processors/congressional_hearing_processor.py
@@ -7,7 +7,11 @@ class CongressionalHearingProcessor(BertProcessor):
     NAME = 'CongressionalHearing'
     NUM_CLASSES = 6
     IS_MULTILABEL = True
-    
+
+    def __init__(self, use_text_b=False):
+        super().__init__()
+        self.use_text_b = use_text_b
+
     def get_train_examples(self, data_dir):
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, 'CongressionalHearing', 'train.tsv')))
@@ -22,10 +26,13 @@ class CongressionalHearingProcessor(BertProcessor):
 
     def _create_examples(self, lines):
         examples = []
+        text_b = None
         for (i, line) in enumerate(lines):
             guid = line[0]
             text_a = line[2]
             label = line[1]
+            if self.use_text_b:
+                text_b = line[3]
             examples.append(
-                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+                InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
