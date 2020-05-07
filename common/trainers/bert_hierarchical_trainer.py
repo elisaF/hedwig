fix loss masking

common/trainers/bert_hierarchical_trainer.py
@@ -70,8 +70,9 @@ class BertHierarchicalTrainer(object):
                 criterion_coarse = criterion_coarse.to(self.args.device)
                 loss_coarse = criterion_coarse(logits_coarse, label_ids_coarse.float())
 
-                criterion_fine = torch.nn.BCEWithLogitsLoss(weight=mask_fine.float(), pos_weight=pos_weights)
+                criterion_fine = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weights)
                 criterion_fine = criterion_fine.to(self.args.device)
+                logits_fine[~mask_fine] = float('-inf')
                 loss_fine = criterion_fine(logits_fine, label_ids.float())
 
             loss_total = loss_coarse + loss_fine