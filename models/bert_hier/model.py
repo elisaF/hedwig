9e7558c2248630090d03702b6060cd6b11b7b614
Elisa Ferracane <elisa@ferracane.com>
2020-05-15 22:45:24 -0500

    create separate classes for each hierarchical model

--- models/bert_hier/model.py
+++ models/bert_hier/model.py
@@ -5,27 +5,13 @@ from transformers.modeling_utils import SequenceSummary
 
 class BertHierarchical(nn.Module):
 
-    def __init__(self, model_family, model_name, num_fine_labels, num_coarse_labels):
+    def __init__(self, model_name, num_fine_labels, num_coarse_labels):
         super().__init__()
 
-        model_map = {
-            'bert': BertModel,
-            'roberta': RobertaModel,
-            'xlnet': XLNetModel
-        }
-        self.model_family = model_family
-        self.bert = model_map[model_family].from_pretrained(model_name, num_labels=num_fine_labels)
-        if self.model_family == 'bert':
-            self.dropout = nn.Dropout(self.bert.config.hidden_dropout_prob)
-            self.classifier_coarse = nn.Linear(self.bert.config.hidden_size, num_coarse_labels)
-            self.classifier_fine = nn.Linear(self.bert.config.hidden_size, num_fine_labels)
-        elif self.model_family == 'roberta':
-            self.dense = nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
-            self.classifier = RobertaClassificationHeads(self.bert.config, num_coarse_labels, num_fine_labels)
-        elif self.model_family == 'xlnet':
-            self.sequence_summary = SequenceSummary(self.bert.config)
-            self.classifier_coarse = nn.Linear(self.bert.config.d_model, num_coarse_labels)
-            self.classifier_fine = nn.Linear(self.bert.config.d_model, num_fine_labels)
+        self.bert = BertModel.from_pretrained(model_name, num_labels=num_fine_labels)
+        self.dropout = nn.Dropout(self.bert.config.hidden_dropout_prob)
+        self.classifier_coarse = nn.Linear(self.bert.config.hidden_size, num_coarse_labels)
+        self.classifier_fine = nn.Linear(self.bert.config.hidden_size, num_fine_labels)
 
     def forward(self,
                 input_ids=None,
@@ -50,22 +36,52 @@ class BertHierarchical(nn.Module):
             inputs_embeds=inputs_embeds,
         )
 
-        if self.model_family == 'roberta':
-            sequence_output = outputs[0]
-            logits_coarse, logits_fine = self.classifier(sequence_output)
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
 
-        elif self.model_family == 'xlnet':
-            output = outputs[0]
-            output = self.sequence_summary(output)
-            logits_coarse = self.classifier_coarse(output)
-            logits_fine = self.classifier_fine(output)
+        logits_coarse = self.classifier_coarse(pooled_output)
+        logits_fine = self.classifier_fine(pooled_output)
 
-        elif self.model_family == 'bert':
-            pooled_output = outputs[1]
-            pooled_output = self.dropout(pooled_output)
+        return logits_coarse, logits_fine
 
-            logits_coarse = self.classifier_coarse(pooled_output)
-            logits_fine = self.classifier_fine(pooled_output)
+
+class RobertaHierarchical(nn.Module):
+
+    def __init__(self, model_name, num_fine_labels, num_coarse_labels):
+        super().__init__()
+
+        self.roberta = RobertaModel.from_pretrained(model_name, num_labels=num_fine_labels)
+        self.dropout = nn.Dropout(self.roberta.config.hidden_dropout_prob)
+        self.classifier_coarse = nn.Linear(self.roberta.config.hidden_size, num_coarse_labels)
+        self.classifier_fine = nn.Linear(self.roberta.config.hidden_size, num_fine_labels)
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            labels=None,
+    ):
+        """
+        a batch is a tensor of shape [batch_size, #file_in_commit, #line_in_file]
+        and each element is a line, i.e., a bert_batch,
+        which consists of input_ids, input_mask, segment_ids, label_ids
+        """
+
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
+        sequence_output = outputs[0]
+        logits_coarse = self.classifier_coarse(sequence_output)
+        logits_fine = self.classifier_fine(sequence_output)
 
         return logits_coarse, logits_fine
 
@@ -88,4 +104,55 @@ class RobertaClassificationHeads(nn.Module):
         output = self.dropout(x)
         logits_coarse = self.classifier_coarse(output)
         logits_fine = self.classifier_fine(output)
+        return logits_coarse, logits_fine
+
+
+class XLNetHierarchical(nn.Module):
+
+    def __init__(self, model_name, num_fine_labels, num_coarse_labels):
+        super().__init__()
+
+        self.transformer = XLNetModel.from_pretrained(model_name, num_labels=num_fine_labels)
+        self.sequence_summary = SequenceSummary(self.transformer.config)
+        self.classifier_coarse = nn.Linear(self.transformer.config.d_model, num_coarse_labels)
+        self.classifier_fine = nn.Linear(self.transformer.config.d_model, num_fine_labels)
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            mems=None,
+            perm_mask=None,
+            target_mapping=None,
+            token_type_ids=None,
+            input_mask=None,
+            head_mask=None,
+            inputs_embeds=None,
+            use_cache=True,
+            labels=None,
+    ):
+        """
+        a batch is a tensor of shape [batch_size, #file_in_commit, #line_in_file]
+        and each element is a line, i.e., a bert_batch,
+        which consists of input_ids, input_mask, segment_ids, label_ids
+        """
+
+        transformer_outputs = self.transformer(
+            input_ids,
+            attention_mask=attention_mask,
+            mems=mems,
+            perm_mask=perm_mask,
+            target_mapping=target_mapping,
+            token_type_ids=token_type_ids,
+            input_mask=input_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+        )
+        output = transformer_outputs[0]
+        output = self.sequence_summary(output)
+
+        logits_coarse = self.classifier_coarse(output)
+        logits_fine = self.classifier_fine(output)
+
         return logits_coarse, logits_fine
