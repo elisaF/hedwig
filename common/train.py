d0a4cd26f8b8c08ff9097db9cb13de9821d06449
Elisa Ferracane <elisa@ferracane.com>
2020-05-26 20:50:52 -0500

    add args to trainer

--- common/train.py
+++ common/train.py
@@ -18,32 +18,32 @@ class TrainerFactory(object):
     }
 
     @staticmethod
-    def get_trainer(dataset_name, model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator, dev_evaluator=None):
+    def get_trainer(dataset_name, model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator, dev_evaluator=None, args=None):
 
         if dataset_name not in TrainerFactory.trainer_map:
             raise ValueError('{} is not implemented.'.format(dataset_name))
 
         return TrainerFactory.trainer_map[dataset_name](
-            model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator, dev_evaluator
+            model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator, dev_evaluator, args
         )
 
     @staticmethod
     def get_trainer_dev(dataset_name, model, embedding, train_loader, trainer_config, train_evaluator,
-                    dev_evaluator):
+                    dev_evaluator, args):
 
         if dataset_name not in TrainerFactory.trainer_map:
             raise ValueError('{} is not implemented.'.format(dataset_name))
 
         return TrainerFactory.trainer_map[dataset_name](
-            model, embedding, train_loader, trainer_config, train_evaluator, dev_evaluator=dev_evaluator
+            model, embedding, train_loader, trainer_config, train_evaluator, dev_evaluator=dev_evaluator, args=args
         )
 
     @staticmethod
-    def get_trainer_test(dataset_name, model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator):
+    def get_trainer_test(dataset_name, model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator, args):
 
         if dataset_name not in TrainerFactory.trainer_map:
             raise ValueError('{} is not implemented.'.format(dataset_name))
 
         return TrainerFactory.trainer_map[dataset_name](
-            model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator=test_evaluator
+            model, embedding, train_loader, trainer_config, train_evaluator, test_evaluator=test_evaluator, args=args
         )
