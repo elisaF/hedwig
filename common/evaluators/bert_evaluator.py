c6386d8c47eab1bb5f4d4c2fd16ee79680bcc0f7
Elisa Ferracane <elisa@ferracane.com>
2020-06-11 11:24:43 -0500

    update trainer and evaluator to pass multilabel arg

--- common/evaluators/bert_evaluator.py
+++ common/evaluators/bert_evaluator.py
@@ -33,7 +33,7 @@ class BertEvaluator(object):
                 self.eval_examples, self.args.max_seq_length, self.tokenizer)
         else:
             eval_features = convert_examples_to_features(
-                self.eval_examples, self.args.max_seq_length, self.tokenizer, use_guid=True)
+                self.eval_examples, self.args.max_seq_length, self.tokenizer, use_guid=True, is_multilabel=self.args.is_multilabel)
 
         unpadded_input_ids = [f.input_ids for f in eval_features]
         unpadded_input_mask = [f.input_mask for f in eval_features]
