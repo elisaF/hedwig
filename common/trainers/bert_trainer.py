94607fabad8aa02c21b7ec5fb83386a8a59f1eb8
Elisa Ferracane <elisa@ferracane.com>
2020-06-16 13:59:56 -0500

    fix logging

--- common/trainers/bert_trainer.py
+++ common/trainers/bert_trainer.py
@@ -36,7 +36,7 @@ class BertTrainer(object):
         self.log_template_classification = ' '.join('{:>5.0f},{:>9.0f},{:>6.0f}/{:<5.0f} {:>6.4f},{:>8.4f},{:8.4f},{:8.4f},{:10.4f}'.split(','))
 
         self.log_header_regression = 'Epoch Iteration Progress   Dev/RMSE   Dev/Kendall   Dev/Pearson Dev/Spearman Dev/Pearson_Spearman Dev/Loss'
-        self.log_template_regression = ' '.join('{:>5.0f},{:>9.0f},{:>6.0f}/{:<5.0f},{:8.4f},{:8.4f},{:8.4f},{:8.4f},,{:8.4f},{:10.4f}'.split(','))
+        self.log_template_regression = ' '.join('{:>5.0f},{:>9.0f},{:>6.0f}/{:<5.0f},{:8.4f},{:8.4f},{:8.4f},{:8.4f},{:8.4f},{:10.4f}'.split(','))
 
         self.iterations, self.nb_tr_steps, self.tr_loss = 0, 0, 0
         self.unimproved_iters = 0
